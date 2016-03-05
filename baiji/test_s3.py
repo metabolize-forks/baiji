b0571705ae937e15759d8fbd3a6c367a0cc79206
Paul Melnikow <paul.melnikow@bodylabs.com>
2016-03-05 09:52:42 -0500

    Clean lint

--- baiji/test_s3.py
+++ baiji/test_s3.py
@@ -1,10 +1,10 @@
 # FIXME pylint: disable=protected-access, unnecessary-lambda, unused-variable
-import mock
 import os
 import shutil
 import tempfile
 import unittest
 import uuid
+import mock
 from baiji import s3
 from baiji.util.testing import create_random_temporary_file
 
