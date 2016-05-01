b4ddebb9e442342fa9d93d5bced44501c3464183
Paul Melnikow <paul.melnikow@bodylabs.com>
2016-05-01 10:43:38 -0400

    Try to make this a namespace package

--- baiji/__init__.py
+++ baiji/__init__.py
@@ -1,1 +1,9 @@
-__version__ = '2.2.4'
+__version__ = '2.2.5'
+
+# Allow other packages to publish modules into the `baiji` namespace.
+#
+# http://stackoverflow.com/a/1676069/893113
+# https://www.python.org/dev/peps/pep-0420/
+
+from pkgutil import extend_path
+__path__ = extend_path(__path__, __name__)
