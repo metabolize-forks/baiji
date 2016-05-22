19b8051619a89eeeefbdea886117e225c6403759
Paul Melnikow <github@paulmelnikow.com>
2016-05-22 16:29:38 -0400

    Check for more failure modes in reachability test (#19)

--- baiji/util/reachability.py
+++ baiji/util/reachability.py
@@ -2,7 +2,10 @@ def internet_reachable():
     import socket
     import requests
     try:
-        requests.get("http://google.com", timeout=1)
-    except (requests.exceptions.Timeout, requests.exceptions.ConnectionError, socket.timeout):
+        requests.get('http://google.com', timeout=1)
+    except (requests.exceptions.Timeout,
+            requests.exceptions.ConnectionError,
+            socket.timeout,
+            socket.gaierror):
         return False
     return True
